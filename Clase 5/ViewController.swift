ac825d228b56f7e26cccae678a5981ce50b4b75a
Alumno <alumno@a51mm-14.local>
2017-11-13 19:04:47 -0500

    Initial Commit

---
+++ Clase 5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clase 5
+//
+//  Created by Alumno on 11/13/17.
+//  Copyright © 2017 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
